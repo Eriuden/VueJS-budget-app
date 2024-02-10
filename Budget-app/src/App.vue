d24b9f1274ae241b934755ed7e00c86f1667b8ae
Eriuden <antoinecarrette@hotmail.fr>
2024-02-10 14:35:52 +0100

    mise en place 4

--- Budget-app/src/App.vue
+++ Budget-app/src/App.vue
@@ -2,17 +2,20 @@
   <Header/>
   <div class="container">
     <Total/>
+    <RevenusDépenses/>
   </div>
 </template>
 
 <script>
   import Header from './components/Header.vue';
   import Total from "./components/Total.vue"
+  import RevenusDépenses from './components/RevenusDépenses.vue';
 
   export default {
     components: {
       Header,
-      Total
+      Total,
+      RevenusDépenses,
     },
   }
 </script>
---
+++ Budget-app/src/components/Transactions.vue
@@ -0,0 +1,16 @@
+<template>
+    <h3>Historique</h3>
+        <ul id="liste" class="liste">
+
+            <li class="moins">
+                Coût de <span>-100 €</span>
+                <button class="delete-button">X</button>
+            </li>
+
+            <li class="plus">
+                Salaire de <span>+1500 €</span>
+                <button class="delete-button">X</button>
+            </li>
+
+        </ul>
+</template>
