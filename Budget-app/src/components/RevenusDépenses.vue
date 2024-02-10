a7051c34dca727dde3b55f02cb78a75a0da357b3
Eriuden <antoinecarrette@hotmail.fr>
2024-02-10 14:23:26 +0100

    miseEnPlace 2

---
+++ Budget-app/src/components/RevenusDépenses.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="revenus-dépenses-container">
+        <div>
+            <h4>Revenus</h4>
+            <p id="revenus" class="revenus">+ 0,00 €</p>
+        </div>
+        <div>
+            <h4>Dépenses</h4>
+            <p id="dépenses" class="dépenses">- 0,00 €</p>
+        </div>
+    </div>
+</template>
