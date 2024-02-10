354154cdc508a5884b4439414b165973381eaf0c
Eriuden <antoinecarrette@hotmail.fr>
2024-02-10 21:10:57 +0100

    Transactions 1

--- Budget-app/src/components/Transactions.vue
+++ Budget-app/src/components/Transactions.vue
@@ -1,16 +1,19 @@
 <template>
     <h3>Historique</h3>
         <ul id="liste" class="liste">
-
-            <li class="moins">
-                Coût de <span>-100 €</span>
+            <li v-for="transaction in transactions"
+            :key="transaction.id"
+            :class="transaction.amount < 0 ? 'moins' : 'plus'">
+                {{transaction.text}} <span>{{ transaction.amount }} €</span>
                 <button class="delete-button">X</button>
             </li>
-
-            <li class="plus">
-                Salaire de <span>+1500 €</span>
-                <button class="delete-button">X</button>
-            </li>
-
         </ul>
 </template>
+
+<script setup>
+               
+const transactions = [
+    {id : 1, text: "exemple", amount: 10}
+]      
+    
+</script>
